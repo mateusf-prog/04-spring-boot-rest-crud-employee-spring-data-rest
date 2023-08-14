90c5c3a2a9c6d04088702eedb8d87c6e64db1b66
mateusf-prog <mateus102006@hotmail.com>
2023-08-14 11:02:11 -0300

    create annotation for custom name of the endpoint - DAO package

--- src/main/java/br/com/mateus/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/br/com/mateus/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package br.com.mateus.cruddemo.dao;
 
 import br.com.mateus.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // no need code
